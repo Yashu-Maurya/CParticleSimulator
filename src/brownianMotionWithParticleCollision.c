c9e915856c96b988e6aae695e28c9381411546e5
Yashu Maurya <yashuff1@gmail.com>
2025-08-09 00:19:58 +0530

    bug fix

--- src/brownianMotionWithParticleCollision.c
+++ src/brownianMotionWithParticleCollision.c
@@ -5,6 +5,9 @@ void brownianMotionWithParticleCollision(Camera3D camera) {
 
     // Initialize all particles
     for (int i = 0; i < PARTICLES_QUANTITY; i += 1) {
+                    Vector3 random_accel = {(float)GetRandomValue(-100, 100) / 100.0f,
+                                    (float)GetRandomValue(-100, 100) / 100.0f,
+                                    (float)GetRandomValue(-100, 100) / 100.0f};
         developParticle(
             &particles[i],
             (Vector3){
@@ -13,7 +16,7 @@ void brownianMotionWithParticleCollision(Camera3D camera) {
                 (float)GetRandomValue(-BOX_CONSTRAINT_Z,
                                       BOX_CONSTRAINT_Z)},  // pos
             (Vector3){0.0f, 0.0f, 0.0f},                   // velocity
-           (Vector3){0.0f, 0.0f, 0.0f},                         // accel
+            (Vector3) random_accel,                        // accel
             (Color){(unsigned char)GetRandomValue(0, 255),
                     (unsigned char)GetRandomValue(0, 255),
                     (unsigned char)GetRandomValue(0, 255), 255},  // color (obv)
@@ -26,13 +29,10 @@ void brownianMotionWithParticleCollision(Camera3D camera) {
         BeginDrawing();
         ClearBackground(BLACK);
         for (int i = 0; i < PARTICLES_QUANTITY; i += 1) {
-            Vector3 random_accel = {(float)GetRandomValue(-100, 100) / 100.0f,
-                                    (float)GetRandomValue(-100, 100) / 100.0f,
-                                    (float)GetRandomValue(-100, 100) / 100.0f};
-            changeParticleAcceleration(&particles[i], random_accel);
+
+
             for (int j = 0; j < PARTICLES_QUANTITY; j += 1) {
-                // for each particle to the initial particle.
-                // checkForCollision(particles, i, j);
+
                 checkForCollision(&particles[i], &particles[j]);
             }
             checkForBoundingBox(&particles[i]);
